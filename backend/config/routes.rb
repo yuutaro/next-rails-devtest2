c7bf58b0f76829b847996999d0300f54c9bab7fb
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-18 19:30:45 +0900

    Userモデル作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      mount_devise_token_auth_for "User", at: "auth"
     end
   end
 end
