a192010d371ffc4f8b5df8f66c014d19b0c52fc9
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-03-18 19:32:24 +0900

    decise日本語化ファイル追加&Rails日本語化設定

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -35,5 +35,8 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # 言語を日本語に設定
+    config.i18n.default_locale = :ja
   end
 end
